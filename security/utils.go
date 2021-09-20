67749d85a45e11f3257112709fed6d5802600efa
jaoks <jeffrey.orihuela@utec.edu.pe>
2021-09-20 15:53:23 -0500

    add         utils

--- security/utils.go
+++ security/utils.go
@@ -4,7 +4,9 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
+	"io"
 )
 
 func createHash(key string) string {
@@ -20,8 +22,45 @@ func createHash(key string) string {
 }
 
 func encrypt(data []byte, passhprase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passhprase)))
+	key := []byte(createHash(passhprase))
+
+	block, _ := aes.NewCipher(key)
+
+	// AEAD (authenticated encryption with associated data) is a cipher mode
+	// providing authenticated encryption with associated data.
 	gcm, _ := cipher.NewGCM(block)
-	nonce := make([]byte, gcm.NonceSize())
-	//https://www.youtube.com/watch?v=jgTqR8PuWuU
+
+	// nonce is a kind of initialization vector
+	// declare size of nonce
+	nonceSize := gcm.NonceSize()
+	// allocates nonce
+	nonce := make([]byte, nonceSize)
+	// fill nonce with random values
+	io.ReadFull(rand.Reader, nonce)
+
+	// Seal encrypts and authenticates plaintext, authenticates the
+	// additional data and appends the result to dst, returning the updated
+	// slice. The nonce must be NonceSize() bytes long and unique for all
+	// time, for a given key.
+	//
+	// To reuse plaintext's storage for the encrypted output, use plaintext[:0]
+	// as dst. Otherwise, the remaining capacity of dst must not overlap plaintext.
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passhprase string) []byte {
+	key := []byte(createHash(passhprase))
+
+	block, _ := aes.NewCipher(key)
+
+	gcm, _ := cipher.NewGCM(block)
+
+	nonceSize := gcm.NonceSize()
+
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+
+	plaintext, _ := gcm.Open(nil, nonce, ciphertext, nil)
+
+	return plaintext
 }
