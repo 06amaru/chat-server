7f88a653f259941584aa41d9af85acf198d966e4
jaoks <jeffreyorihuela@github.com>
2021-11-16 01:15:22 -0500

    style for chat

--- pluggedin-app/src/App.js
+++ pluggedin-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { encrypt, decrypt, PrivateKey } from 'eciesjs'
+import Chat from './chat/Chat'
 
 function App() {
 
@@ -12,22 +13,9 @@ function App() {
   const decrypted = decrypt(k1.toHex(), encrypted).toString()
   console.log(decrypted)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Chat />
+     </div>
   );
 }
 
