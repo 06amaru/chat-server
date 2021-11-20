6e53b9547e4004eb4b7417a4674e7cf746c220c8
Elanny Torres <torres.elanny@gmail.com>
2021-11-20 13:41:57 -0500

    hola mundo

--- pluggedin-ui/src/App.js
+++ pluggedin-ui/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import {ChakraProvider} from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import Chat from './chat/Chat';
 
 
 function App() {
+  console.log("luz");
   return (
     <div className="App">
       <ChakraProvider>
