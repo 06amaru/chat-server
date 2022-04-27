7033cc583265c575f28c1991b5cf7fe8ba42bae0
amaru0601 <amaruorihuela@gmail.com>
2022-04-27 00:43:04 -0500

    [FEAT] add dockercompose

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM golang:alpine
+
+LABEL maintainer="jaoks"
+
+RUN apk update && apk add --no-cache git && apk add --no-cach bash && apk add build-base
+
+RUN mkdir /app
+WORKDIR /app
+
+COPY . .
+COPY .env .
+
+# Download all the dependencies
+RUN go get -d -v ./...
+
+# Install the package
+RUN go install -v ./...
+
+
+RUN go build -o /build
+
+EXPOSE 8080
+
+CMD [ "/build" ]
--- db/client.go
+++ db/client.go
@@ -3,15 +3,25 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/amaru0601/fluent/ent"
 	_ "github.com/lib/pq"
 )
 
 func GetClient() (*ent.Client, error) {
-	client, err := ent.Open("postgres", "host=127.0.0.1 port=5434 user=postgres dbname=postgres password=123456 sslmode=disable")
+
+	// TODO definir variables de entorno para desarrollo local y dockercompose
+	log.Println(os.Getenv("DB_HOST"))
+	log.Println(os.Getenv("DB_PORT"))
+	log.Println(os.Getenv("DB_NAME"))
+	log.Println(os.Getenv("DB_USER"))
+	log.Println(os.Getenv("DB_PASSWORD"))
+
+	client, err := ent.Open("postgres", "host=postgresdb port=5432 user=postgres dbname=postgres password=123456 sslmode=disable")
 	if err != nil {
-		fmt.Printf("failed opening connection to postgres: %v", err)
+		log.Printf("failed opening connection to postgres: %v", err)
 		return nil, err
 	}
 
